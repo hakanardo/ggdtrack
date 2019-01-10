911f5d7e36baf1f798d82ac04c8a5eb61f80f33f
Hakan Ardo <ardo@maths.lth.se>
2019-01-10 17:07:05 +0100

    detetcion class

---
+++ ggdtrack/data/core.py
@@ -0,0 +1,112 @@
+from collections import namedtuple
+
+import cv2
+from shapely import geometry
+import numpy as np
+
+
+class Detection(namedtuple('Detection', ['frame', 'left', 'top', 'right', 'bottom', 'confidence', 'id'])):
+    def draw(self, img, color=None, thickness=3, label=None):
+        if color is None:
+            color = (127,127,127) if self.confidence is None else (0,0,255)
+        cv2.rectangle(img, (int(self.left), int(self.top)), (int(self.right), int(self.bottom)), color, thickness)
+        if label is not None:
+            label = str(label)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.75
+            text_size = cv2.getTextSize(label, font, font_scale, 1)
+            x, y = int(self.left), int(self.top)
+            text_top = (x, y)
+            text_bot = (x + text_size[0][0] + thickness*2, y - text_size[0][1] - thickness*2)
+            text_pos = (x + thickness, y - thickness)
+            cv2.rectangle(img, text_top, text_bot, color, -1)
+            cv2.putText(img, label, text_pos, font, font_scale, (0,0,0), 1, cv2.LINE_AA)
+
+
+    @property
+    def cx(self):
+        return (self.left + self.right) / 2
+
+    @property
+    def cy(self):
+        return (self.top + self.bottom) / 2
+
+    @property
+    def width(self):
+        return abs(self.right - self.left)
+
+    @property
+    def height(self):
+        return abs(self.bottom - self.top)
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    @property
+    def box(self):
+        if not hasattr(self, '_box'):
+            self._box = geometry.box(self.left, self.top, self.right, self.bottom)
+        return self._box
+
+    def iou(self, other):
+        xA = max(self.left, other.left)
+        xB = min(self.right, other.right)
+        if xB < xA:
+            return 0.0
+        yA = max(self.top, other.top)
+        yB = min(self.bottom, other.bottom)
+        interArea = max(0, xB - xA) * max(0, yB - yA)
+        boxAArea = (self.right - self.left) * (self.bottom - self.top)
+        boxBArea = (other.right - other.left) * (other.bottom - other.top)
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+        return iou
+
+    def ioa(self, other):
+        xA = max(self.left, other.left)
+        xB = min(self.right, other.right)
+        if xB < xA:
+            return 0.0
+        yA = max(self.top, other.top)
+        yB = min(self.bottom, other.bottom)
+        interArea = max(0, xB - xA) * max(0, yB - yA)
+        boxAArea = (self.right - self.left) * (self.bottom - self.top)
+        return interArea / float(boxAArea)
+
+    def distance_to(self, other):
+        dx = self.cx - other.cx
+        dy = self.cy - other.cy
+        return np.sqrt(dx**2 + dy**2)
+
+    @property
+    def area(self):
+        return abs(self.right - self.left) * abs(self.bottom - self.top)
+
+    def covers(self, x, y):
+        return self.box.contains(geometry.Point(x, y))
+
+    def interpolate(self, other, f):
+        dt1 = f - other.frame
+        dt2 = self.frame - f
+        dtot = dt2 + dt1
+        left = (dt2 * other.left + dt1 * self.left) / dtot
+        right = (dt2 * other.right + dt1 * self.right) / dtot
+        top = (dt2 * other.top + dt1 * self.top) / dtot
+        bottom = (dt2 * other.bottom + dt1 * self.bottom) / dtot
+        return Detection(f, left, top, right, bottom, None, None)
+
+    def predict(self, df, vx, vy):
+        dx = vx * df
+        dy = vy * df
+        d = Detection(self.frame + df, self.left + dx, self.top + dy, self.right + dx, self.bottom + dy, None, None)
+        return d
+
+    def scale(self, scale):
+        return Detection(self.frame, int(self.left * scale), int(self.top * scale), int(self.right * scale), int(self.bottom * scale), self.confidence, self.id)
+
+    def move(self, dx, dy):
+        return Detection(self.frame, self.left + dx, self.top + dy, self.right + dx, self.bottom + dy, self.confidence, self.id)
+
+    def update_mask(self, mask):
+        mask[self.top:self.bottom, self.left:self.right] = 255
+
---
+++ test/test_data.py
@@ -0,0 +1,57 @@
+from random import randrange
+import numpy as np
+from ggdtrack.data.core import Detection
+
+def slow_iou(self, other):
+    b1 = self.box
+    b2 = other.box
+    return b1.intersection(b2).area / b1.union(b2).area
+
+def random_detection():
+    l = randrange(100)
+    t = randrange(100)
+    w = randrange(1,100)
+    h = randrange(1,100)
+    return Detection(0, l, t, l+w, t+h, 0.9, 7)
+
+class TestData:
+    def test_detetcion(self):
+        d1 = Detection(0, 10, 20, 100, 200, 0.9, 7)
+        assert d1.cx == 55
+        assert d1.cy == 110
+        assert d1.area == 16200
+
+        d2 = Detection(2, 20, 30, 110, 220, 0.9, 7)
+        assert d1.iou(d2) == d2.iou(d1) == slow_iou(d1, d2)
+        assert d1.iou(d1) == d2.iou(d2) == slow_iou(d1, d1) == slow_iou(d1, d1) == 1.0
+
+        assert d1.ioa(d2) == 0.8395061728395061
+        assert d2.ioa(d1) == 0.7953216374269005
+
+        assert d1.distance_to(d2) == d2.distance_to(d1) == 18.027756377319946
+        assert d1.interpolate(d2, 1) == (1, 15, 25, 105, 210, None, None)
+        assert d1.predict(2, 3, 4) == (2, 16, 28, 106, 208, None, None)
+        assert d1.scale(2) == (0, 20, 40, 200, 400, 0.9, 7)
+        assert d1.move(2, 3) == (0, 12, 23, 102, 203, 0.9, 7)
+
+        mask = np.zeros((500, 500))
+        d1.update_mask(mask)
+        d2.update_mask(mask)
+        assert mask.sum() == 5023500
+        assert mask[0, 0] == mask[5, 50] == mask[50, 5] == 0
+        assert mask[20, 10] == 255
+        assert mask[199, 99] == 255
+
+        for _ in range(1000):
+            d1 = random_detection()
+            d2 = random_detection()
+            assert d1.iou(d2) == d2.iou(d1) == slow_iou(d1, d2)
+            assert d1.covers(d1.cx, d1.cy)
+            assert d2.covers(d2.cx, d2.cy)
+
+            mask = np.zeros((500, 500))
+            d1.update_mask(mask)
+            d2.update_mask(mask)
+            assert mask[int(d1.cy), int(d1.cx)] == 255
+            assert mask[int(d2.cy), int(d2.cx)] == 255
+
