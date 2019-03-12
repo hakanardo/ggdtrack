75109a6c5d88a697cebb3ea70bb0706d2416b803
Håkan x Ardö <hakanad@axis.com>
2019-03-12 16:07:21 +0100

    parallellize
    Change-Id: I2926e6b622741941aa1bc1975c46da4e480be2c4

--- ggdtrack/train.py
+++ ggdtrack/train.py
@@ -1,6 +1,7 @@
 import os
 from collections import defaultdict
 from glob import glob
+from queue import Empty
 from random import shuffle, seed
 from shutil import rmtree
 import time
@@ -17,6 +18,9 @@ from ggdtrack.graph_diff import GraphDiffList, make_ggd_batch, split_track_on_mi
 from ggdtrack.klt_det_connect import graph_names
 from ggdtrack.lptrack import lp_track, show_tracks, interpolate_missing_detections, lp_track_weights
 from ggdtrack.utils import default_torch_device, load_graph, promote_graph, demote_graph
+
+import torch.multiprocessing as multiprocessing
+
 
 if True: # Patch pytorch
     string_classes = torch._six.string_classes
@@ -210,6 +214,38 @@ def single_example_passthrough(batch):
     assert len(batch) == 1
     return batch[0]
 
+class WorkerPoolDonwWork: pass
+
+class WorkerPool:
+    def __init__(self, num_workers, worker_fn):
+        self.input_queue = multiprocessing.Queue(num_workers + 1)
+        self.output_queue = multiprocessing.Queue(2 * num_workers + 2)
+        self.workers = []
+        self.worker_fn = worker_fn
+
+        for i in range(num_workers):
+            w = multiprocessing.Process(target=self.worker_loop, daemon=True)
+            self.workers.append(w)
+            w.start()
+
+    def worker_loop(self):
+        while True:
+            work = self.input_queue.get()
+            if work is WorkerPoolDonwWork:
+                break
+            result = self.worker_fn(work)
+            self.output_queue.put(result)
+
+    def __del__(self):
+        for _ in self.workers:
+            self.input_queue.put(WorkerPoolDonwWork)
+
+    def get(self, block=True, timeout=None):
+        return self.output_queue.get(block, timeout)
+
+    def put(self, work, block=True, timeout=None):
+        self.input_queue.put(work, block, timeout)
+
 def train_frossard(dataset, logdir, model, mean_from=None, device=default_torch_device, limit=None, epochs=1000, resume_from=None):
 
     if mean_from is None and resume_from is None:
@@ -254,57 +290,82 @@ def train_frossard(dataset, logdir, model, mean_from=None, device=default_torch_
         shuffle(entries)
         entries = entries[:limit]
 
-    train_data = EvalGtGraphs(dataset, entries, '-%s-eval_graph' % model.feature_name)
-    train_loader = DataLoader(train_data, 1, True, collate_fn=single_example_passthrough, num_workers=6)
+    threads = multiprocessing.cpu_count() - 2
 
-    for epoch in range(start_epoch, start_epoch + epochs):
-        shuffle(entries)
-        epoch_hamming_distance = 0
-        for graph, detection_weight_features, connection_batch in tqdm(train_loader, "Epoch %s" % epoch, disable=True):
-            connection_batch = connection_batch.to(device)
-            detection_weight_features = detection_weight_features.to(device)
-            promote_graph(graph)
+    train_data = EvalGtGraphs(dataset, entries, '-%s-eval_graph' % model.feature_name)
+    train_loader = DataLoader(train_data, 1, True, collate_fn=single_example_passthrough, num_workers=threads)
+
+    def worker(work):
+        graph, connection_weights, detection_weights, entry_weight, connection_batch, detection_weight_features = work
+        promote_graph(graph)
+        lp_track_weights(graph, connection_weights, detection_weights, entry_weight, add_gt_hamming=True)
+        demote_graph(graph)
+        return graph, connection_batch, detection_weight_features
+
+    lp_tracker_pool = WorkerPool(threads, worker)
+
+    epoch_hamming_distance = batch_count = 0
+    epoch = start_epoch
+    while True:
+    #     shuffle(entries)
+        for graph, detection_weight_features, connection_batch in train_loader:
+        # for graph, detection_weight_features, connection_batch in train_data:
 
             model.eval()
-            connection_weights = model.connection_batch_forward(connection_batch)
-            detection_weights = model.detection_model(detection_weight_features)
-            tracks = lp_track_weights(graph, connection_weights, detection_weights, model.entry_weight, add_gt_hamming=True)
-            # tracks = lp_track(graph, connection_batch, detection_weight_features, model, add_gt_hamming=True)
-            # interpolate_missing_detections(tracks)
-            # show_tracks(scene, tracks, gt_graph_frames)
+            connection_weights = model.connection_batch_forward(connection_batch.to(device))
+            detection_weights = model.detection_model(detection_weight_features.to(device))
 
-            model.train()
-            optimizer.zero_grad()
+            # promote_graph(graph)
+            # tracks = lp_track_weights(graph, connection_weights, detection_weights, model.entry_weight, add_gt_hamming=True)
+            lp_tracker_pool.put((graph, connection_weights.detach().cpu(), detection_weights.detach().cpu(), model.entry_weight, connection_batch, detection_weight_features))
+            while True:
+                try:
+                    graph, connection_batch, detection_weight_features = lp_tracker_pool.get(block=False)
+                except Empty:
+                    break
 
-            hamming_distance_present = hamming_distance_entry = hamming_distance_connect = loss = 0
-            connection_weights = model.connection_batch_forward(connection_batch)
-            detection_weights = model.detection_model(detection_weight_features)
-            for det in graph:
-                assert len(det.next) == len(det.outgoing)
-                loss += (det.present.value - det.gt_present) * detection_weights[det.index]
-                loss += (det.entry.value - det.gt_entry) * model.entry_weight_parameter
-                loss += sum(connection_weights[i] * (v.value - gt)
-                            for v, i, gt in zip(det.outgoing, det.weight_index, det.gt_next))
-                hamming_distance_present += det.present.value != det.gt_present
-                hamming_distance_entry += det.entry.value != det.gt_entry
-                hamming_distance_connect += sum(v.value != gt for v, gt in zip(det.outgoing, det.gt_next))
-            epoch_hamming_distance += hamming_distance_present + hamming_distance_entry + hamming_distance_connect
+                if batch_count >= len(train_data):
+                    snapp = {
+                        'epoch': epoch,
+                        'model_state': model.state_dict(),
+                        'optimizer_state': optimizer.state_dict(),
+                        'train_hamming': epoch_hamming_distance,
+                    }
+                    torch.save(snapp, os.path.join(logdir, "snapshot_%.3d.pyt" % epoch))
+                    print('%3d Hamming:' % epoch, epoch_hamming_distance)
+                    epoch_hamming_distance = batch_count = 0
+                    epoch += 1
+                    if epoch  >= start_epoch + epochs:
+                        return
+                    continue
+                batch_count += 1
 
-            # print(loss.item(), hamming_distance_present, hamming_distance_entry, hamming_distance_connect)
-            loss.backward()
-            # print(model.entry_weight_parameter, model.entry_weight_parameter.grad, hamming_distance_entry)
-            optimizer.step()
+                promote_graph(graph)
 
-        snapp = {
-            'epoch': epoch,
-            'model_state': model.state_dict(),
-            'optimizer_state': optimizer.state_dict(),
-            'loss': loss,
-            'train_hamming': epoch_hamming_distance,
-        }
-        torch.save(snapp, os.path.join(logdir, "snapshot_%.3d.pyt" % epoch))
-        print('%3d Hamming:' % epoch, epoch_hamming_distance)
+                # interpolate_missing_detections(tracks)
+                # show_tracks(scene, tracks, gt_graph_frames)
 
+                model.train()
+                optimizer.zero_grad()
+
+                hamming_distance_present = hamming_distance_entry = hamming_distance_connect = loss = 0
+                connection_weights = model.connection_batch_forward(connection_batch.to(device))
+                detection_weights = model.detection_model(detection_weight_features.to(device))
+                for det in graph:
+                    assert len(det.next) == len(det.outgoing)
+                    loss += (det.present.value - det.gt_present) * detection_weights[det.index]
+                    loss += (det.entry.value - det.gt_entry) * model.entry_weight_parameter
+                    loss += sum(connection_weights[i] * (v.value - gt)
+                                for v, i, gt in zip(det.outgoing, det.weight_index, det.gt_next))
+                    hamming_distance_present += det.present.value != det.gt_present
+                    hamming_distance_entry += det.entry.value != det.gt_entry
+                    hamming_distance_connect += sum(v.value != gt for v, gt in zip(det.outgoing, det.gt_next))
+                epoch_hamming_distance += hamming_distance_present + hamming_distance_entry + hamming_distance_connect
+
+                # print(loss.item(), hamming_distance_present, hamming_distance_entry, hamming_distance_connect)
+                loss.backward()
+                # print(model.entry_weight_parameter, model.entry_weight_parameter.grad, hamming_distance_entry)
+                optimizer.step()
 
     # writer = SummaryWriter(logdir)
 
@@ -313,6 +374,7 @@ if __name__ == '__main__':
     from ggdtrack.duke_dataset import Duke
     from ggdtrack.model import NNModelGraphresPerConnection
     from ggdtrack.eval import prep_eval_graphs
+    t0 = time.time()
 
     dataset = Duke('data')
     # train_graphres_minimal(dataset, "logdir", NNModelGraphresPerConnection())
@@ -321,5 +383,6 @@ if __name__ == '__main__':
     seed(42)
     # train_frossard(dataset, "cachedir/logdir_fossard", NNModelGraphresPerConnection(), resume_from="cachedir/logdir_fossard", limit=1)
 
-    # train_frossard(dataset, "cachedir/logdir_fossard2", NNModelGraphresPerConnection(), mean_from="cachedir/logdir/snapshot_009.pyt", limit=1)
-    train_frossard(dataset, "cachedir/logdir_fossard2", NNModelGraphresPerConnection(), resume_from="cachedir/logdir/snapshot_009.pyt", limit=1)
+    train_frossard(dataset, "cachedir/logdir_fossard2", NNModelGraphresPerConnection(), mean_from="cachedir/logdir/snapshot_009.pyt", limit=10)
+    # train_frossard(dataset, "cachedir/logdir_fossard2", NNModelGraphresPerConnection(), resume_from="cachedir/logdir/snapshot_009.pyt", epochs=10)
+    print(time.time() - t0)
