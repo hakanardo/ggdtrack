60dd1381e738ba3df4de5577d32b6c32afafaa02
Håkan x Ardö <hakanad@axis.com>
2019-03-12 16:16:08 +0100

    from scratch
    Change-Id: If516ebc399d7903d3ee85fb64f91eed56e3136c1

--- fossard_run.py
+++ fossard_run.py
@@ -23,8 +23,8 @@ def main(datadir):
     model = NNModelGraphresPerConnection()
 
     prep_eval_graphs(dataset, NNModelGraphresPerConnection(), parts=["train"])
-    train_frossard(dataset, "cachedir/logdir_fossard", model, resume_from="cachedir/logdir", epochs=10)
-    # train_frossard(dataset, "cachedir/logdir_fossard", model, mean_from="cachedir/logdir/snapshot_009.pyt", epochs=1000)
+    # train_frossard(dataset, "cachedir/logdir_fossard", model, resume_from="cachedir/logdir", epochs=10)
+    train_frossard(dataset, "cachedir/logdir_fossard", model, mean_from="cachedir/logdir/snapshot_009.pyt", epochs=1000)
 
     prep_eval_tracks(dataset, "cachedir/logdir_fossard", model, 'eval', threads=1)
     res, res_int = eval_prepped_tracks(dataset, 'eval')
