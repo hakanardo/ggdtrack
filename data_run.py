d95b73bdabe93e816d30e6c4e50e72bca42d2a75
Hakan Ardo <hakan@debian.org>
2019-03-19 06:48:36 +0100

    gt

--- data_run.py
+++ data_run.py
@@ -1,7 +1,7 @@
 from glob import glob
 
 from ggdtrack.duke_dataset import Duke
-from ggdtrack.eval import prep_eval_tracks, eval_prepped_tracks, ConnectionBatch
+from ggdtrack.eval import prep_eval_tracks, eval_prepped_tracks, ConnectionBatch, prep_eval_gt_tracks
 from ggdtrack.model import NNModelGraphresPerConnection
 from ggdtrack.train import train_graphres_minimal
 
@@ -9,7 +9,13 @@ def main():
     max_extra = 3
     dataset = Duke("data")
 
-    for train_amount in [0.01, 0.1, 1.0]:
+    prep_eval_gt_tracks(dataset, NNModelGraphresPerConnection)
+    res, res_int = eval_prepped_tracks(dataset, 'eval')
+    open("cachedir/eval_gt_results.txt", "w").write(res)
+    open("cachedir/eval_gt_results_int.txt", "w").write(res_int)
+
+
+    for train_amount in [0.001, 0.0001, 0.00001]:
         for itt in range(10):
             logdir = "cachedir/logdir_%4.2f_%.2d" % (train_amount, itt)
             model = NNModelGraphresPerConnection()
