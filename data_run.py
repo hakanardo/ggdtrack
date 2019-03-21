01d9ee90a10b55bfcd7300ad393747c2627327cf
Hakan Ardo <hakan@debian.org>
2019-03-21 20:00:09 +0100

    fix

--- data_run.py
+++ data_run.py
@@ -9,13 +9,8 @@ def main():
     max_extra = 3
     dataset = Duke("data")
 
-    prep_eval_gt_tracks(dataset, NNModelGraphresPerConnection)
-    res, res_int = eval_prepped_tracks(dataset, 'eval')
-    open("cachedir/eval_gt_results.txt", "w").write(res)
-    open("cachedir/eval_gt_results_int.txt", "w").write(res_int)
 
-
-    for train_amount in [0.001, 0.0001, 0.00001]:
+    for train_amount in [0.0001, 0.001, 0.01, 0.1, 1.0]:
         for itt in range(10):
             logdir = "cachedir/logdir_%8.6f_%.2d" % (train_amount, itt)
             model = NNModelGraphresPerConnection()
@@ -27,6 +22,12 @@ def main():
             res, res_int = eval_prepped_tracks(dataset, 'eval')
             open(logdir + "/eval_results.txt", "w").write(res)
             open(logdir + "/eval_results_int.txt", "w").write(res_int)
+
+    prep_eval_gt_tracks(dataset, NNModelGraphresPerConnection)
+    res, res_int = eval_prepped_tracks(dataset, 'eval')
+    open("cachedir/eval_gt_results.txt", "w").write(res)
+    open("cachedir/eval_gt_results_int.txt", "w").write(res_int)
+
 
 if __name__ == '__main__':
     main()
