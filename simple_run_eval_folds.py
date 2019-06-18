423c669d7442ce1a201bbbefdbb996742ddaaf17
Håkan x Ardö <hakanad@axis.com>
2019-06-18 14:43:25 +0200

    cachedir
    Change-Id: I3e85728605872e61590abf7957dbb6197ed90469

--- simple_run_eval_folds.py
+++ simple_run_eval_folds.py
@@ -1,18 +1,20 @@
 from ggdtrack.eval import eval_prepped_tracks_joined
 from ggdtrack.mot16_dataset import Mot16
 
+cachedir='/lunarc/nobackup/projects/lu-haar/ggdtrack/cachedir.1/'
+
 print('Linear')
-datasets = [Mot16("data", fold=fold) for fold in range(4)]
+datasets = [Mot16("data", fold=fold, cachedir=cachedir) for fold in range(4)]
 eval_prepped_tracks_joined(datasets)
 
 print('NNModelSimpleMLP1')
-datasets = [Mot16("data", fold=fold) for fold in range(4)]
+datasets = [Mot16("data", fold=fold, cachedir=cachedir) for fold in range(4)]
 for d in datasets:
     d.logdir += '_NNModelSimpleMLP1'
 eval_prepped_tracks_joined(datasets)
 
 print('NNModelSimpleMLP2')
-datasets = [Mot16("data", fold=fold) for fold in range(4)]
+datasets = [Mot16("data", fold=fold, cachedir=cachedir) for fold in range(4)]
 for d in datasets:
     d.logdir += '_NNModelSimpleMLP2'
 eval_prepped_tracks_joined(datasets)
