32de60135581f32c75c76933994464a13132379a
Hakan Ardo <hakan@debian.org>
2020-02-28 22:39:18 +0100

    more

--- ablation.py
+++ ablation.py
@@ -32,12 +32,12 @@ def main(dataset, datadir, threads, segment_length, cachedir, minimal_confidence
     find_minimal_graph_diff.too_short_track = too_short_track
     find_minimal_graph_diff.long_track = too_short_track * 2
 
-    prep_training_graphs(dataset, cachedir, limit_train_amount=1e-3, threads=threads, segment_length_s=segment_length,
+    prep_training_graphs(dataset, cachedir, limit_train_amount=1e-2, threads=threads, segment_length_s=segment_length,
                          worker_params=dict(max_connect=max_connect))
 
     global_skip = {"LongConnectionOrder", "LongFalsePositiveTrack"}
     model = NNModelGraphresPerConnection()
-    prep_minimal_graph_diffs(dataset, model, threads=threads) #, skipped_ggd_types=global_skip)
+    prep_minimal_graph_diffs(dataset, model, threads=threads, skipped_ggd_types=global_skip)
     prep_eval_graphs(dataset, model, threads=threads)
 
     train_graphres_minimal(dataset, model, epochs=epochs, max_worse_eval_epochs=max_worse_eval_epochs)
