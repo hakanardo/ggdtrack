0539a37b6baae61b06f6f85c727e5b46d564212e
Håkan x Ardö <hakanad@axis.com>
2020-03-02 15:25:53 +0100

    ablatian
    Change-Id: I47dd6ae8d8492bb496d260062a9cd32b1a0d4a76

--- ablation.py
+++ ablation.py
@@ -11,6 +11,32 @@ from ggdtrack.klt_det_connect import prep_training_graphs
 from ggdtrack.model import NNModelGraphresPerConnection
 from ggdtrack.train import train_graphres_minimal
 
+global_skip = {
+    "LongConnectionOrder",
+    "LongFalsePositiveTrack"
+}
+
+ggd_types = {
+    'FalsePositive',
+    'SplitFromFalsePositive',
+    'ExtraFirst',
+    'DualFalsePositive',
+    'DetectionSkipp',
+    'SkippLast',
+    'Split',
+    'SkippFirst',
+    'SplitToFalsePositive',
+    'ExtraLast',
+    'IdSwitch',
+    'DoubleSplitAndMerge',
+    'Merge',
+    'SplitAndMerge',
+    'TooShortTrack',
+    'LongTrack',
+    'LongConnectionOrder',
+    'LongFalsePositiveTrack',
+}
+
 @click.command()
 @click.option("--dataset", default="Duke", help="Dataset loader class (Mot16, Duke or VisDrone)")
 @click.option("--datadir", default="data", help="Directory into which the Duke dataset will be downloaded")
@@ -23,33 +49,37 @@ from ggdtrack.train import train_graphres_minimal
 @click.option("--max-worse-eval-epochs", default=float('Inf'), type=float)
 @click.option("--epochs", default=10, type=int)
 @click.option("--too-short-track", default=2, type=int)
-def main(dataset, datadir, threads, segment_length, cachedir, minimal_confidence, fold, max_connect, max_worse_eval_epochs, epochs, too_short_track):
+@click.option("--logdir-prefix", default="", help="Prepended to logdir path")
+def main(dataset, datadir, threads, segment_length, cachedir, minimal_confidence, fold, max_connect,
+         max_worse_eval_epochs, epochs, too_short_track, logdir_prefix):
     opts = dict(cachedir=cachedir, default_min_conf=minimal_confidence)
     if fold is not None:
         opts['fold'] = fold
     dataset = eval(dataset)(datadir, **opts)
+    logdir = logdir_prefix + '/' + dataset.logdir
 
     find_minimal_graph_diff.too_short_track = too_short_track
     find_minimal_graph_diff.long_track = too_short_track * 2
 
-    prep_training_graphs(dataset, cachedir, limit_train_amount=1, threads=threads, segment_length_s=segment_length,
-                         worker_params=dict(max_connect=max_connect))
 
-    global_skip = {"LongConnectionOrder", "LongFalsePositiveTrack"}
-    model = NNModelGraphresPerConnection()
-    prep_minimal_graph_diffs(dataset, model, threads=threads, skipped_ggd_types=global_skip)
-    prep_eval_graphs(dataset, model, threads=threads)
+    for skipped in ggd_types:
+        if skipped in global_skip:
+            continue
 
-    train_graphres_minimal(dataset, model, epochs=epochs, max_worse_eval_epochs=max_worse_eval_epochs)
+        prep_training_graphs(dataset, cachedir, limit_train_amount=0.1, threads=threads, segment_length_s=segment_length,
+                             worker_params=dict(max_connect=max_connect))
 
-    prep_eval_tracks(dataset, model, 'eval', threads=1)
-    res, res_int = eval_prepped_tracks(dataset, 'eval')
-    open(os.path.join(dataset.logdir, "eval_results.txt"), "w").write(res)
-    open(os.path.join(dataset.logdir, "eval_results_int.txt"), "w").write(res_int)
+        dataset.logdir = logdir + "_skipped_" + skipped
+        model = NNModelGraphresPerConnection()
+        prep_minimal_graph_diffs(dataset, model, threads=threads, skipped_ggd_types=global_skip.union([skipped]))
+        prep_eval_graphs(dataset, model, threads=threads)
 
-    #res, res_int = eval_prepped_tracks_joined(dataset, 'eval')
-    #open(os.path.join(dataset.logdir, "eval_results_joined.txt"), "w").write(res)
-    #open(os.path.join(dataset.logdir, "eval_results_joined_int.txt"), "w").write(res_int)
+        train_graphres_minimal(dataset, model, epochs=epochs, max_worse_eval_epochs=max_worse_eval_epochs)
+
+        prep_eval_tracks(dataset, model, 'eval', threads=1)
+        res, res_int = eval_prepped_tracks(dataset, 'eval')
+        open(os.path.join(dataset.logdir, "eval_results.txt"), "w").write(res)
+        open(os.path.join(dataset.logdir, "eval_results_int.txt"), "w").write(res_int)
 
 if __name__ == '__main__':
     main()
